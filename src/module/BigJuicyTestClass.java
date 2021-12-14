e9560a86089c6ee17f87d9978d733223220d1217
kegliitis <62723953+kegliitis@users.noreply.github.com>
2021-12-14 14:26:09 +0100

    whatever

--- src/module/BigJuicyTestClass.java
+++ src/module/BigJuicyTestClass.java
@@ -9,8 +9,8 @@ public class BigJuicyTestClass
     modelManager.assignStudentsToCourses();
     Student student = new Student("123451", "Alley Heya", 1, "Y");
     Student student2 = new Student("531552", "PPPP", 4, "X");
-    modelManager.addStudentToClass("531552", "4X");
-    System.out.println(modelManager.getClassById("4X"));
+    modelManager.addSession(new Session(new MyDate(17, 12, 2021), new TimeInterval(820, 1150), modelManager.getAllClassrooms()
+        .getClassroom("C05.15"), modelManager.getCourse("SEP1X")));
     modelManager.export();
   }
 }
