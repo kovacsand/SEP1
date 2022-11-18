0951f577b801a616a7ce3b85e650594a8d40ba54
James Cullen <jcullen.dk@gmail.com>
2022-11-18 13:28:55 +0100

    Added Awesomesauce from 2x to the list of students

--- src/module/BigJuicyTestClass.java
+++ src/module/BigJuicyTestClass.java
@@ -8,6 +8,7 @@ public class BigJuicyTestClass
     modelManager.importData();
     Student student = new Student("123451", "Alley Heya", 1, "Y");
     Student student2 = new Student("531552", "PPPP", 4, "X");
+    Student student3 = new Student("121212", "Awesomesauce", 100,"2x");
     modelManager.addSession(new Session(new MyDate(17, 12, 2021), new TimeInterval(820, 1150), modelManager.getAllClassrooms()
         .getClassroom("C05.15"), modelManager.getCourse("SEP1X")));
 
